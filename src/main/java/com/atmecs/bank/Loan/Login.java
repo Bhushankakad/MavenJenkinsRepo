114dea9d916d9719fe9bd701f19cb7cbf6bbf74e
Bhushan Kakad <bhushan.kakad@atmecs.com>
2019-01-18 13:11:52 +0530

    we are in project

---
+++ src/main/java/com/atmecs/bank/Loan/Login.java
@@ -0,0 +1,12 @@
+package com.atmecs.bank.Loan;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		
+		System.out.println("Hello Jenkins");
+	}
+
+}
